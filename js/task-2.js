00fbdeb169e54dd65e049826c9b1751ee2322c47
Polina-mind <popova.v.polina@gmail.com>
2021-02-05 16:52:10 +0200

    Update

---
+++ js/task-2.js
@@ -0,0 +1,46 @@
+//ЗАДАНИЕ 2
+
+//Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback,
+//а принимала всего два параметра allUsers и userName и возвращала промис.
+
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
+  const updatedUsers = allUsers.map((user) =>
+    user.name === userName ? { ...user, active: !user.active } : user
+  );
+
+  //   callback(updatedUsers);
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(updatedUsers);
+    }, 0);
+  });
+
+  return promise;
+};
+
+const logger = (updatedUsers) => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+toggleUserState(users, "Mango", logger);
+toggleUserState(users, "Lux", logger);
+
+/*
+ * Должно работать так
+ */
+
+setTimeout(() => {
+  console.log("%cЗАДАНИЕ 2:", "color: green; font-size: 18px");
+
+  toggleUserState(users, "Mango").then(logger);
+  toggleUserState(users, "Lux").then(logger);
+}, 3000);
